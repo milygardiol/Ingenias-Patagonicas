ca4d82c520121b1b31b4ca86cef1d05517fdb150
Mily <gardiol72@gmail.com>
2024-06-18 10:10:32 -0300

    js - bienvenida

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+// Solicitar el nombre del usuario
+let nombre = prompt("Por favor, ingresa tu nombre:");
+
+// Verificar si el usuario ingresó un nombre
+if (nombre) {
+    // Mostrar un mensaje de agradecimiento personalizado
+    alert("¡Gracias por visitar nuestro sitio web y nuestra Provincia, " + nombre + "!" + " Esperamos que disfrutes tu visita.");
+} else {
+    // Mostrar un mensaje de agradecimiento general si no se ingresó un nombre
+    alert("¡Gracias por visitar nuestro sitio web y nuestra Provincia! Esperamos que disfrutes tu visita.");
+}
